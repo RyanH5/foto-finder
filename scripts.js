47e1c1f663c47c56fd25705a1447f06709a6223e
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-01-16 13:41:06 -0700

    Add camera sound effect to submit button

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
-$('#add_image__file').on('click', addCardToPage);
+var camera = new Audio ("images/camera-sound.mp3");
+$('#add_image__file').on('click', prependCard);
 
 
-function addCardToPage() {
-  $('#photo_album__section').prepend(`<article><h2>${$('#photo_create_title__input').val()}</h2><img src=""><p>${$('#photo_create_caption__input')}</p><footer><footer><input type="submit" class="trash_can__btn"><input type="submit class="heart__btn"</footer></article>`)
+
+function prependCard(event) {
+  event.preventDefault();
+  $('#photo_album__section').prepend(`<article class="prepend_card"><h2 class="prepend_card__title">${$('#photo_create_title__input').val()}</h2><img src="" class="prepend_card__image"><p>${$('#photo_create_caption__input').val()}</p><footer><input type="submit" class="remove_btn"><input type="submit" class="favorite_btn"</footer></article>`)
+  console.log('click')
+  camera.play();
 }
